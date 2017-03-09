44133e146171f8f5f407107273567d6b3a6151d8
Ryan Siu <rsiu@stuy.edu>
2017-03-08 20:17:57 -0500

    hw15

--- HW15/QuickSort.java
+++ HW15/QuickSort.java
@@ -1,23 +1,30 @@
+// Ryan Siu
+// APCS2 pd5
+// HW15 -- So So Quick
+// 2017-03-08
+
 /*****************************************************
  * class QuickSort
  * Implements quicksort algo to sort an array of ints in place
  *
  * 1. Summary of QuickSort algorithm:
- * QSort(arr): 
+ * QSort(arr): Run partition on an array, break the array into subarrays on
+ *             either side of the number placed in the correct index, and 
+ *             run partition on each of the subarrays. Repeat.
  *
  * 2a. Worst pivot choice / array state and associated runtime: 
+ *     When the chosen pivot value is moved to either extreme of the array.
+ *     O(n^2)
  *
- * 2b. Best pivot choice / array state and associated runtime:
+ * 2b. Best pivot choice / array state and associated runtime: 
+ *     When the chosen pivot value is moved to the middle of the array.
+ *     O(nlogn)
  *
- * 3. Approach to handling duplicate values in array:
+ * 3. Approach to handling duplicate values in array: 
+ *    Duplicates are already handled by the partition method; they are put to
+ *     the left of the partitioned array.
  *
  *****************************************************/
-
-/***
-    PROTIP: Assume no duplicates during initial development phase.
-    Once you have a working implementation, test against arrays 
-    with duplicate values, and revise if necessary. (Backup first.)
- ***/
 
 public class QuickSort 
 {
@@ -71,11 +78,12 @@ public class QuickSort
     // Insert your auxiliary helper methods here.
 
 
-    // 
+    // Helper method for wrapper
     public static void qsort( int[] d, int left, int right ) 
     { 
-	if (left > right) {
-	    int pvtPos = partition( d, left, right, (left + right) / 2);
+	if (left < right) {
+	    int pvtPos = partition( d, left, right, left); // last param doesn't matter
+	    System.out.println(pvtPos);
 	    qsort( d, left, pvtPos - 1);
 	    qsort( d, pvtPos + 1, right);
 	}
@@ -113,8 +121,6 @@ public class QuickSort
        	System.out.println("arr1 after qsort: " );
 	printArr(arr1);
 
-	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
-
 	// randomly-generated arrays of n distinct vals
 	int[] arrN = new int[10];
 	for( int i = 0; i < arrN.length; i++ )
@@ -130,10 +136,6 @@ public class QuickSort
 	qsort( arrN );
 	System.out.println("arrN after sort: " );
 	printArr(arrN);
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-
-	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
 
 	//get-it-up-and-running, static test case w/ dupes:
 	int [] arr2 = {7,1,5,12,3,7};
@@ -160,7 +162,6 @@ public class QuickSort
 	qsort( arrMatey );
 	System.out.println("arrMatey after sort: " );
 	printArr(arrMatey);
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
     }//end main
 
